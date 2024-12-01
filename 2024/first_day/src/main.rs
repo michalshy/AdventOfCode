f8ea0a7ae795b9856b9317e68aa6ddd2e9b66980
Michin <niesmialymichal@gmail.com>
2024-12-01 19:37:37 +0100

    day one completed!

--- 2024/first_day/src/main.rs
+++ 2024/first_day/src/main.rs
@@ -3,11 +3,7 @@ use std::collections::BTreeMap;
 
 mod input;
 
-pub fn decrement_first(mut side: BTreeMap<i32, i32>, key: &i32) {
-    side.get_mut(key);
-}
-
-fn main() {
+pub fn part_one() {
     let mut total: i64 = 0;
     let mut left_side: BTreeMap<i64, i64> = BTreeMap::new();
     let mut right_side: BTreeMap<i64, i64> = BTreeMap::new();
@@ -38,7 +34,7 @@ fn main() {
                         println!("{} - {}", r_key, l_key);
                         total += i64::abs(l_key - r_key);
                         *right_side.get_mut(r_key).unwrap() -= 1;
-                        match right_side.get(r_key).unwrap() {
+                        match *right_side.get_mut(r_key).unwrap() {
                             0 => {
                                 right_side.pop_first(); 
                             },
@@ -54,5 +50,46 @@ fn main() {
         }
     }
 
-    print!("{}", total);
+    println!("{}", total);
+}
+
+pub fn part_two() {
+    let mut total: i64 = 0;
+    let mut left_side: BTreeMap<i64, i64> = BTreeMap::new();
+    let mut right_side: BTreeMap<i64, i64> = BTreeMap::new();
+
+    for line in INPUT.split('\n') {
+        let mut el = line.split("   ");
+        let left = el.next().unwrap().parse::<i64>().unwrap();
+        let right = el.next().unwrap().parse::<i64>().unwrap();
+        match left_side.get_mut(&left) {
+            Some(val) => *val = *val + 1,
+            None => {
+                left_side.insert(left, 1);
+            },
+        }
+        match right_side.get_mut(&right) {
+            Some(val) => *val = *val + 1,
+            None => {
+                right_side.insert(right, 1);
+            },
+        }
+    }
+
+    for (l_key, _l_val) in left_side {
+        let appearances = right_side.get(&l_key);
+        match appearances {
+            Some(val) => {
+                total += l_key * val;
+            },
+            _ => (),
+        }
+    }
+
+    println!("{}", total);
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
